8633f6afd4f91369944d6d56d0bfb5177e1aed7b
Alexey <lexac7078@gmail.com>
2025-04-15 10:41:39 +0500

    Etap 2

---
+++ Etap2/src/etap2/Etap2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package etap2;
+
+/**
+ *
+ * @author Home
+ */
+public class Etap2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Savinih Alexey Aleksandrovich");
+        System.out.println("");
+        System.out.println("Ras");
+        System.out.println("Dva");
+        System.out.println("Tri");
+        System.out.println("Chetire");
+        System.out.println("Pyat");
+        System.out.println("Shest");
+        System.out.println("");
+        int age = 25;
+        System.out.print("Alexey ");
+        System.out.println("19");
+        System.out.println("Victor " + age);
+        
+    }
+    
+}
